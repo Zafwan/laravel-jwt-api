c7f50ac635bfa9c8c1d5e52f374f55d4d5f0c132
zafwan-xox <mohd.zafwan@xox.com.my>
2023-09-05 14:52:52 +0800

    Make homepage

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fun with code</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+</head>
+<body>
+    <div class="center-both">
+        <img class="astronaut" src="{{asset('img/astronaut.svg')}}">
+        <a class="link" href="{{url('/api/documentation')}}">API DOCUMENTATION<br><h2 class="cta">Learn More</h2></a>
+    </div>
+</body>
+</html>
